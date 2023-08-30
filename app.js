81475d225ee8f8db8694b8d4ad443c65521e9534
amelieboydshire <aboydshire10@gmail.com>
2023-08-30 18:51:07 +0100

    added SetUp() function

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+let countries = [];
+
+
 const fetchRestCountries = async () => {
-    const reponse = await fetch("https://restcountries.com/v3.1/all");
+    const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
-}
+
+    const countryList = document.querySelector("#country-list");
+
+
+jsonData.forEach(countryData => {
+    const restCountries = document.createElement("li");
+    restCountries.textContent = countryData.name.common;
+    countryList.appendChild(restCountries);
+
+});
+
+document.querySelector("body").appendChild(countryList);
+
+};
+
+fetchRestCountries();
