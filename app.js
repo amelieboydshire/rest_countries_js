02ba807c8e7fc0a63bcb0d8b01bd91e3ea24a1bb
amelieboydshire <aboydshire10@gmail.com>
2023-08-30 19:15:08 +0100

    created new HTML elements

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ let countries = [];
 const fetchRestCountries = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
+    countries = jsonData;
+    SetUp(); // Call setUp function after fetching data
 
+};
+    
+    const populateCountryList = () => {
     const countryList = document.querySelector("#country-list");
 
+    // Remove original <p> element if exists
+    const originalParagraph = document.querySelector("#original-paragraph");
+    if (originalParagraph) {
+        originalParagraph.remove();
+    }
 
-jsonData.forEach(countryData => {
+
+    // Populate the list with country information
+    countries.forEach(countryData => {
     const restCountries = document.createElement("li");
-    restCountries.textContent = countryData.name.common;
+    restCountries.textContent = `Country: ${countryData.name.common}, Population: ${countryData.population}`;
     countryList.appendChild(restCountries);
 
 });
 
-document.querySelector("body").appendChild(countryList);
-
 };
 
-fetchRestCountries();
+const SetUp = () => {
+    fetchRestCountries(); 
+    populateCountryList(); 
+  };
+
+  window.addEventListener("DOMContentLoaded", SetUp);
